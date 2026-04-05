5681e19fbdddc7b5cd4cce52597308ee2ead4806
spec <48649161+sp-ec@users.noreply.github.com>
2026-04-05 18:26:14 -0400

    added basic calendar view with draggable events

--- frontend/src/pages/CalenderViewPage.jsx
+++ frontend/src/pages/CalenderViewPage.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { React, useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 function CalenderViewPage() {
+  const [events, setEvents] = useState([]);
+
+  const handleSelect = (selectionInfo) => {
+    const newEvent = {
+      id: String(Date.now()),
+      start: selectionInfo.startStr,
+      end: selectionInfo.endStr,
+      allDay: selectionInfo.allDay,
+      backgroundColor: "#3b82f6",
+    };
+
+    setEvents([...events, newEvent]);
+
+    const calendarApi = selectionInfo.view.calendar;
+    calendarApi.unselect();
+  };
+
   return (
-    <div>Calender View Page</div>
-  )
+    <div>
+      <FullCalendar
+        plugins={[timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        slotDuration="00:15:00"
+        slotLabelInterval="00:60:00"
+        slotLabelFormat={{
+          hour: "numeric",
+          minute: "2-digit",
+          omitZeroMinute: false,
+          meridiem: "short",
+        }}
+        slotMinTime="06:00:00"
+        slotMaxTime="24:00:00"
+        allDaySlot={false}
+        selectable={true}
+        selectMirror={true}
+        unselectAuto={true}
+        select={handleSelect}
+        events={events}
+      />
+    </div>
+  );
 }
 
-export default CalenderViewPage
+export default CalenderViewPage;
--- frontend/src/pages/JoinGroupPage.jsx
+++ frontend/src/pages/JoinGroupPage.jsx
@@ -1,17 +1,37 @@
-/*import React from 'react'
+import React from "react";
+import "./JoinGroupPage.css";
 
 function JoinGroupPage() {
+  let username;
+
+  document.getElementById("submitNameButton").onClick = function () {
+    username = document.getElementById("username").value;
+    document.getElementById("welcome").textContent = username;
+  };
+
   return (
-    <div>Join Group Page</div>
-  )
+    <div>
+      <div>
+        <h1 id="myH1">Welcome</h1>
+
+        <label>Name: </label>
+        <input id="username"></input>
+        <br />
+        <button id="submitNameButton">submit</button>
+
+        <br />
+        <h3 id="groupMembers"> Group Members: </h3>
+        <p id="welcome"></p>
+      </div>
+
+      <br />
+      <div>
+        <button id="goToCalendar" onClick="CalenderViewPage.jsx">
+          Go To Calendar
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default JoinGroupPage*/
-
-let username;
-
-document.getElementById("submitNameButton").onclick = function(){
-	username = document.getElementById("username").value;
-	document.getElementById("welcome").textContent = username;
-}
-
+export default JoinGroupPage;
